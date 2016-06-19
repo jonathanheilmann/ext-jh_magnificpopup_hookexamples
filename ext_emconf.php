f420122eee5426200eee121184fdece97c6f150c
jonathanheilmann <mail@jonathan-heilmann.de>
2016-06-19 16:10:20 +0200

    [TASK] clean up ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,30 +11,30 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-  'title' => 'Magnific Popup - Hook Examples',
-  'category' => 'doc',
-  'state' => 'beta',
-  'uploadfolder' => true,
-  'createDirs' => '',
-  'clearcacheonload' => true,
-  'author' => 'Jonathan Heilmann',
-  'author_email' => 'mail@jonathan-heilmann.de',
-  'author_company' => '',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
+    'title' => 'Magnific Popup - Hook Examples',
     'description' => 'This extension provides some working examples for hooks in EXT:jh_magnificpopup. Please see documentation of EXT:jh_magnificpopup (Chapter Development) and have a look at the commented code of this extension.',
+    'category' => 'doc',
     'version' => '0.5.0-dev',
+    'state' => 'beta',
+    'uploadfolder' => true,
+    'createDirs' => '',
+    'clearcacheonload' => true,
+    'author' => 'Jonathan Heilmann',
+    'author_email' => 'mail@jonathan-heilmann.de',
+    'author_company' => '',
+    'constraints' =>
+        array (
+            'depends' =>
+                array (
                     'typo3' => '6.2.0-7.6.99',
                     'jh_magnificpopup' => '0.5.0',
+                ),
+            'conflicts' =>
+                array (
+                ),
+            'suggests' =>
+                array (
+                ),
+        ),
 );
 
