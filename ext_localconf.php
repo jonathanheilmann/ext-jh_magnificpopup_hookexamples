0ef414326812012df53c2f73038665b54088f149
jonathanheilmann <mail@jonathan-heilmann.de>
2016-06-19 16:16:55 +0200

    [TASK] Remove ExtensionBuilder files (Resolves #3)

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
-}
-
-## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
 
 // Register hook EidTypeHook for jh_magnificpopup
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jh_magnificpopup']['EidTypeHook'][] = 'EXT:jh_magnificpopup_hookexamples/Classes/Hooks/EidTypeHook.php:Heilmann\JhMagnificpopupHookexamples\Hooks\EidTypeHook->ajax';
